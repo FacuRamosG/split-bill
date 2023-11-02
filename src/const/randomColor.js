8a9facb8c902c93a545b79213df3065949c228c6
facuramosg <facundoramosgarde@gmail.com>
2023-11-02 13:47:10 -0300

    fix random colors

--- src/const/randomColor.js
+++ src/const/randomColor.js
@@ -1,8 +1,15 @@
 export default function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    const minBrightness = 100;
+
+    function getRandomComponent() {
+      return Math.floor(Math.random() * (255 - minBrightness + 1)) + minBrightness;
     }
+  
+    const red = getRandomComponent();
+    const green = getRandomComponent();
+    const blue = getRandomComponent();
+  
+    const color = `rgb(${red}, ${green}, ${blue})`;
+  
     return color;
   }
